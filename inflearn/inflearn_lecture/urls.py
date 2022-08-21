7546f8826aa615e4788fab323d92500fc4e28a38
HRPzz <royalpkgh@naver.com>
2022-08-21 21:50:00 +0900

    lecture list

--- inflearn/inflearn_lecture/urls.py
+++ inflearn/inflearn_lecture/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_list, name='home_list'),  # views.py 와 연결
+
+    path('lecture_list/', views.lecture_list, name='lecture_list'),
 ]
--- inflearn/inflearn_lecture/views.py
+++ inflearn/inflearn_lecture/views.py
@@ -5,3 +5,10 @@ from .models import myText
 def home_list(request):
     texts = myText.objects.filter()  # db 에 있는 myText 데이터를 모두 가져옴
     return render(request, 'inflearn_lecture/home_list.html', {'texts': texts})  # templates/inflearn_lecture/home_list.html 와 연결, texts 라는 변수에 담아서 myText 데이터를 보냄
+
+def lecture_list(request):
+    texts = myText.objects.filter()
+    hot_lecture = myText.objects.filter(category="인기")
+    return render(request, 'inflearn_lecture/lecture_list.html',
+                {'texts': texts, 'hot_lecture': hot_lecture}
+                )
