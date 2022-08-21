94b8f01b35a6a2be9672048879979599caeda8c6
HRPzz <royalpkgh@naver.com>
2022-08-21 17:23:06 +0900

    app url

--- inflearn/inflearn_lecture/views.py
+++ inflearn/inflearn_lecture/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_list(request):
+    return render(request, 'inflearn_lecture/home_list.html')  # templates/inflearn_lecture/home_list.html 와 연결
