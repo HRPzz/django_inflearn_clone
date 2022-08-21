666af3c13cfc0178e7d16c493b06065ef48d3ce4
HRPzz <royalpkgh@naver.com>
2022-08-21 20:42:58 +0900

    django template language

--- inflearn/inflearn_lecture/views.py
+++ inflearn/inflearn_lecture/views.py
@@ -3,5 +3,5 @@ from .models import myText
 
 # Create your views here.
 def home_list(request):
-    texts = myText.objects.filter(category="HTML")  # db 에 있는 카테고리가 HTML 인 myText 데이터를 모두 가져옴
+    texts = myText.objects.filter()  # db 에 있는 myText 데이터를 모두 가져옴
     return render(request, 'inflearn_lecture/home_list.html', {'texts': texts})  # templates/inflearn_lecture/home_list.html 와 연결, texts 라는 변수에 담아서 myText 데이터를 보냄
