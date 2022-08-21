04cf9e4727da940fdddc888fa6e8810a4116ccb7
HRPzz <royalpkgh@naver.com>
2022-08-21 19:14:06 +0900

    admin myText model

--- inflearn/inflearn_lecture/views.py
+++ inflearn/inflearn_lecture/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import myText
 
 # Create your views here.
 def home_list(request):
-    return render(request, 'inflearn_lecture/home_list.html')  # templates/inflearn_lecture/home_list.html 와 연결
+    texts = myText.objects.filter()  # db 에 있는 myText 데이터를 모두 가져옴
+    return render(request, 'inflearn_lecture/home_list.html', {'texts': texts})  # templates/inflearn_lecture/home_list.html 와 연결, texts 라는 변수에 담아서 myText 데이터를 보냄
